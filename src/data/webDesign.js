ae93a74c1a2a9bdacf74e4ac106cde6717ce041d
lydiob7 <lydiob7@tutanota.com>
2023-09-26 07:20:18 -0300

    Update: websites and resume

--- src/data/webDesign.js
+++ src/data/webDesign.js
@@ -13,44 +13,57 @@ const array = [
         status: "ready"
     },
     {
-        id: "vaikuntha",
-        website: "https://vaikuntha.com.ar/",
-        title: "Vaikuntha",
+        id: "chameleonic",
+        website: "https://chameleonic.io",
+        title: "Chameleonic",
         en: {
-            subtitle: "Ceramist portfolio"
+            subtitle: "Consultancy agency"
         },
         es: {
-            subtitle: "Portfolio de Ceramista"
-        },
-        type: "web-design",
-        status: "ready"
-    },
-    {
-        id: "miguel-bruno",
-        website: "https://miguelbruno.es/",
-        title: "Miguel Bruñó",
-        en: {
-            subtitle: "Musician portfolio"
-        },
-        es: {
-            subtitle: "Portfolio de Músico"
-        },
-        type: "web-design",
-        status: "ready"
-    },
-    {
-        id: "tomi-scattini",
-        website: "https://tomiscattini.com/",
-        title: "Tomi Scattini",
-        en: {
-            subtitle: "Full stack developer portfolio"
-        },
-        es: {
-            subtitle: "Portfolio de Desarrollador full-stack"
+            subtitle: "Agencia de consultorías"
         },
         type: "web-design",
         status: "ready"
     }
+    // {
+    //     id: "vaikuntha",
+    //     website: "https://vaikuntha.com.ar/",
+    //     title: "Vaikuntha",
+    //     en: {
+    //         subtitle: "Ceramist portfolio"
+    //     },
+    //     es: {
+    //         subtitle: "Portfolio de Ceramista"
+    //     },
+    //     type: "web-design",
+    //     status: "ready"
+    // },
+    // {
+    //     id: "miguel-bruno",
+    //     website: "https://miguelbruno.es/",
+    //     title: "Miguel Bruñó",
+    //     en: {
+    //         subtitle: "Musician portfolio"
+    //     },
+    //     es: {
+    //         subtitle: "Portfolio de Músico"
+    //     },
+    //     type: "web-design",
+    //     status: "ready"
+    // },
+    // {
+    //     id: "tomi-scattini",
+    //     website: "https://tomiscattini.com/",
+    //     title: "Tomi Scattini",
+    //     en: {
+    //         subtitle: "Full stack developer portfolio"
+    //     },
+    //     es: {
+    //         subtitle: "Portfolio de Desarrollador full-stack"
+    //     },
+    //     type: "web-design",
+    //     status: "ready"
+    // }
 ];
 
 export default array;
